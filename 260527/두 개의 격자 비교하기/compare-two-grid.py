a50e4e7b3ab93fb9587eb3952f9b68cc29199826
minsikk <131092169+1224kang@users.noreply.github.com>
2026-05-27 20:06:19 +0900

    [Passed] 두 개의 격자 비교하기 (47ms, 16MB)

---
+++ 260527/두 개의 격자 비교하기/compare-two-grid.py
@@ -0,0 +1,12 @@
+N,M=map(int,input().split())
+arr1=[list(map(int,input().split())) for _ in range(N)]
+arr2=[list(map(int,input().split())) for _ in range(N)]
+
+for i in range(N):
+    new_arr=[]
+    for j in range(M):
+        if(arr1[i][j]==arr2[i][j]):
+            new_arr.append(0)
+        else:
+            new_arr.append(1)
+    print(*new_arr)
