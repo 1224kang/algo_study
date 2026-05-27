ca56567e911e1d4f89b4b34fb3dd395d93fd14c7
minsikk <131092169+1224kang@users.noreply.github.com>
2026-05-27 21:21:41 +0900

    [Passed] 함수를 이용해 직사각형 만들기 (78ms, 17MB)

---
+++ 260527/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+# Please write your code here.
+def make_arr(n,m):
+    for i in range(n):
+        for j in range(m):
+            print(1,end="")
+        print()
+
+make_arr(n,m)
